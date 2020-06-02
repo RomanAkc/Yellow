9ad0da35a3481d492462b99ad7a28b47b2674db3
Roman Demin <sporco1988@gmail.com>
2020-06-03 00:35:00 +0700

    Add week_03_task_01

---
+++ week_03_task_01/Source.cpp
@@ -0,0 +1,12 @@
+#include "sum_reverse_sort.h"
+#include "iostream"
+
+int main()
+{
+	std::cout << Sum(2, 3) << std::endl;
+	std::cout << Reverse("bla") << std::endl;
+	std::vector<int> v{ 1, 5, 2 };
+	Sort(v);
+	std::cout << std::endl;
+	return 0;
+}
